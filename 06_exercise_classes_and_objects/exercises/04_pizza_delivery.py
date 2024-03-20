9062572613d468038050ae3e43a2e3fea3cffe4c
Aleksandar Spasov <147007999+AleksandarSpasov2@users.noreply.github.com>
2024-03-20 07:56:54 +0200

    updates

--- 06_exercise_classes_and_objects/exercises/04_pizza_delivery.py
+++ 06_exercise_classes_and_objects/exercises/04_pizza_delivery.py
@@ -3,11 +3,8 @@ class PizzaDelivery:
     def __init__(self, name, price, ingredients):
         self.name = name
         self.price = price
-        self.ingredients = ingredients
+        self.ingredients = {}
         self.ordered = False
 
-    def add_extra(self, ingredient: str, quantity: int, price_per_quantity: float):
-        if ingredient in self.ingredients.keys():
-            self.ingredients[ingredient] += quantity
-        else:
-            self.ingredients[ingredient] = quantity
+    def add_extra(self, ingredient, quantity, price_per_quantity):
+        self.ingredients[ingredient] = self.ingredients.get(ingredient, 0) + quantity
