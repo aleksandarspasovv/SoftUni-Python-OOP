3cf7070b2ad2006e5c137926e85e789ff2ed1da5
Aleksandar Spasov <147007999+AleksandarSpasov2@users.noreply.github.com>
2024-03-25 05:39:31 +0200

    updates

---
+++ 08_exercise_inheritance/exercises/05_shop/project/product_repository.py
@@ -0,0 +1,28 @@
+from project.drink import Drink
+from project.food import Food
+
+
+class ProductRepository:
+
+    def __init__(self):
+        self.products = []
+
+    def add(self, product):
+        self.products.append(product)
+
+    def find(self, product_name):
+        for product in self.products:
+            if product.name == product_name:  # Check for product.name instead
+                return product
+        # return next((product for product in self.products if product.name == product_name), None)
+
+    def remove(self, product_name):
+        product = self.find(product_name)
+        if product:
+            self.products.remove(product)
+
+        if product:
+            self.products.remove(product)
+
+    def __repr__(self):
+        return '\n'.join(f'{p.name}: {p.quantity}' for p in self.products)
