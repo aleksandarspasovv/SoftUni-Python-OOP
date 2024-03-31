dc41f68ea8f4951a508007535092be80d4b8765a
Aleksandar Spasov <147007999+AleksandarSpasov2@users.noreply.github.com>
2024-03-31 06:26:12 +0300

    Update library.py

--- 06_exercise_classes_and_objects/exercises/task_liblary/project/library.py
+++ 06_exercise_classes_and_objects/exercises/task_liblary/project/library.py
@@ -1,7 +1,12 @@
+from project.user import User
+
+
 class Library:
 
     def __init__(self):
         self.user_records = []
         self.books_available = {}
         self.rented_books = {}
+
+    def get_book(self, author: str, book_name: str, days_to_return: int, user: User):
         
