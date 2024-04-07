be824bfb963bf556eb58279aa3efaeb4fd393b1c
Aleksandar Spasov <147007999+AleksandarSpasov2@users.noreply.github.com>
2024-04-07 07:40:45 +0300

    updates

--- 12_exercise_static_and_class_methods/01_photo_album.py
+++ 12_exercise_static_and_class_methods/01_photo_album.py
@@ -1,2 +1,8 @@
 class PhotoAlbum:
-    
+    MAX_PAGES = 4
+
+    def __init__(self, pages):
+        self.pages = pages
+        self.photos = []
+    def from_photos_count(self, photos_count):
+        return
