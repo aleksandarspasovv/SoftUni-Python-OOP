a160985f99891b411ca5e4ca61a7c2fcf2f6941e
Aleksandar Spasov <147007999+AleksandarSpasov2@users.noreply.github.com>
2024-04-27 05:19:51 +0300

    Update hotel.py

--- 11_static_and_class_methods/hotel_rooms_2/project/hotel.py
+++ 11_static_and_class_methods/hotel_rooms_2/project/hotel.py
@@ -1,2 +1,26 @@
+from typing import List
+
+from project.room import Room
+
+
 class Hotel:
-    
+
+    def __init__(self, name):
+        self.name = name
+        self.rooms: List[Room] = []
+        self.guests = 0
+
+    @classmethod
+    def from_stars(cls, stars_count):
+        return cls(f"{stars_count} stars Hotel")
+
+    def add_room(self, room: Room):
+        self.rooms.append(room)
+
+    def take_room(self, room_number):
+        try:
+            room = next(filter(lambda r: r.number == room_number, self.rooms))
+        except StopIteration:
+            return
+
+        if
