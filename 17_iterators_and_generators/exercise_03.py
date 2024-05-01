79fe55d9d139570fbec8a28749d0410663665386
AleksandarSpasov2 <147007999+AleksandarSpasov2@users.noreply.github.com>
2024-05-01 10:13:33 +0300

    Create exercise_03.py

---
+++ 17_iterators_and_generators/exercise_03.py
@@ -0,0 +1,21 @@
+class vowels:
+
+    def __init__(self, string):
+        self.string = string
+        self.vowels_list = ['a', 'e', 'o', 'u', 'y', 'i']
+        self.vowels_values = [x for x in self.string if x.lower() in self.vowels_list]
+        self.index = -1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.index += 1
+        if self.index < len(self.vowels_values):
+            return self.string[self.index]
+        raise StopIteration
+
+
+my_string = vowels('Abcedifuty0o')
+for char in my_string:
+    print(char)
